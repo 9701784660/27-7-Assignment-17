6a65c330255b60e97fd6722691296a8938328f0c
praveen4858 <praveenryali048@gmail.com>
2023-07-27 15:25:57 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_7_Assignment_17
+{
+    class Program
+    {
+        static void Main()
+        {
+            Circle circle = new Circle(8);
+            Rectangle rectangle = new Rectangle(9, 6);
+            Triangle triangle = new Triangle(3, 5, 7);
+
+            Console.WriteLine("Circle Info:");
+            circle.PrintInfo();
+            Console.WriteLine();
+
+            Console.WriteLine("Rectangle Info:");
+            rectangle.PrintInfo();
+            Console.WriteLine();
+
+            Console.WriteLine("Triangle Info:");
+            triangle.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_7_Assignment_17
+{
+    // Derived class representing a rectangle
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height) : base("Rectangle")
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area => Width * Height;
+
+        public override double Perimeter => 2 * (Width + Height);
+    }
+
+
+}
+
+
